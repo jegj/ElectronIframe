2d70df243092260ddb81f0d6f6e94f3d5e7f1267
Javier <jegj57@gmail.com>
2017-09-08 12:16:39 -0500

    First version

---
+++ main.js
@@ -0,0 +1,108 @@
+// Module to create native browser window.
+const {Menu, Tray, app, BrowserWindow} = require('electron')
+
+const path = require('path')
+const url = require('url')
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow
+
+function createWindow () {
+	const template = [
+		{
+			label: 'DealerWorks',
+			click: function () {
+				mainWindow.show();
+			}
+		},
+		{
+			label: 'Settings',
+		},
+		{
+			label: 'Logout',
+			click: function() {
+				mainWindow.loadURL(url.format({
+					pathname: path.join(__dirname, 'login.html'),
+					protocol: 'file:',
+					slashes: true
+				}))
+			}
+		},
+		{
+			label: 'Exit',
+			click: function () {
+				app.isQuiting = true;
+				app.quit();
+			}
+		}
+	];
+	// THIS COULD BE SMALL IMAGE
+	const iconPath = path.join(__dirname, '/images/icon-128x128.png');
+	const appIcon = new Tray(iconPath);
+	const contextMenu = Menu.buildFromTemplate(template);
+	appIcon.setToolTip('DealerworksLauncher');
+	appIcon.setContextMenu(contextMenu);
+	// Create the browser window.
+	mainWindow = new BrowserWindow(
+		{
+			width: 700, 
+			height: 700,
+			icon: path.join(__dirname, 'images/icons/icon-256x256.ico'), 
+			title: 'Dealerbuilt', frame: true
+		}
+	);
+
+	// and load the index.html of the app.
+	mainWindow.loadURL(url.format({
+		pathname: path.join(__dirname, 'index.html'),
+		protocol: 'file:',
+		slashes: true
+	}))
+
+	mainWindow.on('minimize',function(event){
+		event.preventDefault()
+				mainWindow.hide();
+	});
+
+	mainWindow.on('close', function (event) {
+			if( !app.isQuiting){
+					event.preventDefault()
+					mainWindow.hide();
+			}
+			return false;
+	});
+
+	// Emitted when the window is closed.
+	mainWindow.on('closed', function () {
+		// Dereference the window object, usually you would store windows
+		// in an array if your app supports multi windows, this is the time
+		// when you should delete the corresponding element.
+		mainWindow = null
+	});
+}
+
+// const trayControl = new TrayControl(app, iconPath, template);
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+	// On OS X it is common for applications and their menu bar
+	// to stay active until the user quits explicitly with Cmd + Q
+	if (process.platform !== 'darwin') {
+		// app.quit()
+	}
+});
+
+app.on('activate', function () {
+	// On OS X it's common to re-create a window in the app when the
+	// dock icon is clicked and there are no other windows open.
+	if (mainWindow === null) {
+		createWindow()
+	}
+});
+
